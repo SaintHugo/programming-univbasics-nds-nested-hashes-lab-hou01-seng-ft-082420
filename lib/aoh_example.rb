d8e249b8b46c719d8c662e7f7194790691a529b6
Hugo Rodrigez <hugor37@gmail.com>
2020-08-08 18:54:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -39,7 +39,7 @@ def aoh_lookup(aoh, row, key)
     { :name =>  "Kate Gompert", :occupation => "None" },
     { :name =>  "Bruce Green", :occupation => "Fan of Mildred" }
   ]
-  [aoh][row][key]
+  characters[2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
